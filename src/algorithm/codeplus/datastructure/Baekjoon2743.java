7d725007913429184474cf1458ba733874895b72
osulas <hkkwon940@gmail.com>
2022-02-23 22:37:59 +0900

    백준2743 단어 길이 재기

---
+++ src/algorithm/codeplus/datastructure/Baekjoon2743.java
@@ -0,0 +1,16 @@
+package algorithm.codeplus.datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2743 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.print(s.length());
+    }
+}
