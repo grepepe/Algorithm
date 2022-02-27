b4938cb8a0ba83671c21b14ea019a7c5041f7667
osulas <hkkwon940@gmail.com>
2022-02-27 18:18:25 +0900

    백준15649 N과 M (1)

--- README.md
+++ README.md
@@ -77,3 +77,4 @@
 * [백준14500. 테트로미노](https://www.acmicpc.net/problem/14500)
 * [백준6064. 카잉 달력](https://www.acmicpc.net/problem/6064)
 * [백준1748. 수 이어 쓰기 1](https://www.acmicpc.net/problem/1748)
+* [백준15649. N과 M (1)](https://www.acmicpc.net/problem/15649)
---
+++ src/algorithm/codeplus/bruteforce/Baekjoon15649.java
@@ -0,0 +1,43 @@
+package algorithm.codeplus.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon15649 {
+
+    private static final StringBuilder sb = new StringBuilder();
+    private static int[] arr;
+    private static int bitMask = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        arr = new int[input[1]];
+
+        select(input[0], input[1], 0);
+
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.print(sb);
+    }
+
+    private static void select(int n, int m, int cnt) {
+        if (cnt == m) {
+            for (int a : arr) {
+                sb.append(a).append(" ");
+            }
+            sb.deleteCharAt(sb.length()-1).append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if ((bitMask & (1 << i)) == 0) {
+                bitMask |= (1 << i);
+                arr[cnt] = i;
+                select(n, m, cnt+1);
+                bitMask &= ~(1 << i);
+            }
+        }
+    }
+}
