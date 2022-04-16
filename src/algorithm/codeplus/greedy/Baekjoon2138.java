c9a67f5cba856d99446a1214f21ab71fff679e20
osulas <hkkwon940@gmail.com>
2022-04-16 20:46:23 +0900

    백준2138 전구와 스위치 입력만 추가

---
+++ src/algorithm/codeplus/greedy/Baekjoon2138.java
@@ -0,0 +1,18 @@
+package algorithm.codeplus.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2138 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[n];
+        boolean[] map = new boolean[n];
+        int ans = 0;
+        
+    }
+}
