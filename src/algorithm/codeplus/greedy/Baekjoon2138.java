2f551b5e21b45494ece691db54da44a4f5c8051a
osulas <hkkwon940@gmail.com>
2022-04-18 15:12:40 +0900

    백준2138 전구와 스위치

--- src/algorithm/codeplus/greedy/Baekjoon2138.java
+++ src/algorithm/codeplus/greedy/Baekjoon2138.java
@@ -9,10 +9,55 @@ public class Baekjoon2138 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ans = 100000;
         int n = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[n];
-        boolean[] map = new boolean[n];
-        int ans = 0;
-        
+        char[] input1 = br.readLine().toCharArray();
+        char[] input2 = br.readLine().toCharArray();
+        boolean[] arr1 = new boolean[n];
+        boolean[] arr2 = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            arr1[i] = input1[i] != input2[i];
+            arr2[i] = input1[i] != input2[i];
+        }
+
+        arr2[0] = !arr2[0];
+        arr2[1] = !arr2[1];
+
+        int tmp = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr1[i - 1]) {
+                arr1[i - 1] = !arr1[i - 1];
+                arr1[i] = !arr1[i];
+                if (i < n - 1) {
+                    arr1[i + 1] = !arr1[i + 1];
+                }
+                tmp++;
+            }
+        }
+
+        if (!arr1[n - 1]) {
+            ans = Math.min(ans, tmp);
+        }
+
+        tmp = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr2[i - 1]) {
+                arr2[i - 1] = !arr2[i - 1];
+                arr2[i] = !arr2[i];
+                if (i < n - 1) {
+                    arr2[i + 1] = !arr2[i + 1];
+                }
+                tmp++;
+            }
+        }
+
+        if (!arr2[n - 1]) {
+            ans = Math.min(ans, tmp);
+        }
+
+        System.out.print(ans == 100000 ? -1 : ans);
     }
 }
