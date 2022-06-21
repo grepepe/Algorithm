0c57ac8413d18318350ef6213426304cca68e198
osulas <hkkwon940@gmail.com>
2022-06-21 23:39:08 +0900

    백준1300 K번째 수

---
+++ src/algorithm/codeplus/binarysearch/Baekjoon1300.java
@@ -0,0 +1,17 @@
+package algorithm.codeplus.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon1300 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        br.close();
+    }
+}
